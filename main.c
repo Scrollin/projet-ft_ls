0fab03ef53c5fa6d4249ae55f9ffe34a116447c9
mgrondin <grondin.maxime@hotmail.fr>
2014-11-17 18:23:12 +0100

    add closedir

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mgrondin <mgrondin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/17 13:35:41 by mgrondin          #+#    #+#             */
-/*   Updated: 2014/11/17 17:47:36 by mgrondin         ###   ########.fr       */
+/*   Updated: 2014/11/17 18:22:11 by mgrondin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,6 @@ int		main(int argc, char **argv)
 	if(!(rep = opendir(".")))
 	   return (0);
 	ft_ls(rep, argv[1]);
+	closedir(rep);
 	return (0);
 }
